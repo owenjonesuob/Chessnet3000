d483569a30d87ca21470f87353c8c783859df3c6
owenjonesuob <olj23@bath.ac.uk>
2017-07-18 17:13:50 +0100

    Add ambiguous move distinction to parse_move; finish move_R

--- parser.py
+++ parser.py
@@ -45,30 +45,41 @@ def unroll(board):
 
 def parse_move(move):
     
-    # Strip down to just the move
+    # Remove capture/check notation (x/+)
     pattern = re.compile("[\Wx]+") 
-    move = pattern.sub("", move)
+    move = pattern.sub("", move)    
     
     piece_dict = {"R": 1, "N": 2, "B": 3, "Q": 4, "K": 5}
     
+    # Identify piece type
     if move[0].isupper():
         d = piece_dict[move[0]]
         move = move[1:]
     else:
         d = 0
+        
+    # Check for ambiguity in move
+    if len(move) == 3:
+        ambig = move[0]
+        move = move[1:]
+    elif len(move) > 3:
+        return -1
+    else:
+        ambig = None
+     
     
     rank_dict = {"a": 7, "b": 6, "c": 5, "d": 4, "e": 3, "f": 2, "g": 1, "h": 0}
     
     r, f = rank_dict[move[0]], int(move[1])-1
     
-    return [r, f, d]
+    return [r, f, d, ambig]
 
     
 def move_simple(to, player)
     """
     Wipes old position and adds new position of the single piece in a layer
     """
-    r, f, d = to
+    r, f, d, ambig = to
     board[:, :, d] = np.multiply(board[:, :, d],
                                  np.array([board[:, :, d] != player]))
     board[r, f, d] = player
@@ -78,24 +89,32 @@ def move_KQ(to, player):
     
 
 def move_R(to, player):
-    r, f, d = to
+    r, f, d, ambig = to
    
-    if ([board[:, :, d] == player].sum() == 1):
-        move_simple(to, player)
+    if np.array([board[:, :, d] == player]).sum() == 1:
+        return move_simple(to, player)
         
-    elif ([board[:, f, d] == player].sum() == 1):
+    elif np.array([board[:, f, d] == player]).sum() == 1:
         board[:, f, d] = np.multiply(board[:, f, d],
                                      np.array([board[:, f, d] != player]))
-        board[r, f, d] = player
         
-    elif ([board[r, :, d] == player].sum() == 1):
+    elif np.array([board[r, :, d] == player]).sum() == 1:
         board[r, :, d] = np.multiply(board[r, :, d],
                                      np.array([board[r, :, d] != player]))
-        board[r, f, d] = player
                 
     else:
-        return -1
+        try:
+            a = int(ambig)-1
+            board[:, a, d] = np.multiply(board[:, a, d],
+                                         np.array([board[:, a, d] != player]))
+            
+        except ValueError:
+            rank_dict = {"a": 7, "b": 6, "c": 5, "d": 4, "e": 3, "f": 2, "g": 1, "h": 0}
+            a = rank_dict(ambig)
+            board[a, :, d] = np.multiply(board[a, :, d],
+                                         np.array([board[a, :, d] != player]))
         
+    board[r, f, d] = player
         
     
 def move_N(to, player):
